5b42241a3fce85939143599e684bec77953fa802
danitw <danidevpy@gmail.com>
2022-09-21 18:23:04 -0300

    making routes system and resolving cors

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"crud-backend/routes"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
 
 func main() {
-  fmt.Println("Hello, World!")
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.Use(mux.CORSMethodMiddleware(router))
+
+	routes.Routes(router)
+
+  c := cors.New(cors.Options{
+    AllowedOrigins: []string{"*"},
+    AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
+    AllowedHeaders: []string{"*"},
+  })
+
+	log.Println("initializing server in port 8000")
+	log.Fatal(http.ListenAndServe(":8000", c.Handler(router)))
 }
